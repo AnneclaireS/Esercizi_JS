82573c08eb2fcebd493c9b88487d2578d1878891
AnneclaireS <annasantoriello15@gmail.com>
2022-08-03 20:39:37 +0200

    added ex 1 to 13

--- Esercizio13/exercise-3.js
+++ Esercizio13/exercise-3.js
@@ -1,19 +1,14 @@
-const sum = function (a, b) {
-  return a + b;
-}
+const sum = (a, b) => a + b
 
-const subtract = function (a, b) {
-  return a - b;
-}
+const subtract = (a, b) => a - b;
 
-const multiply = function (a, b) {
-  return a * b;
-}
 
-const divide = function (a, b) {
-  return a / b;
-}
+const multiply = (a, b) => a * b;
 
-const log = function (value) {
-  console.log(value);
-}
+
+const divide = (a, b) => a / b;
+
+const log = (value) => console.log(value);
+
+log(((2 + 4) * (5 + 2) - 2) / 5);
+  
